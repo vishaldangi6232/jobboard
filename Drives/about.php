d9c03e8ec7b4152b35bb40e770f2a0cf4f726656
vishaldangi6232 <vishal566dangi@gmail.com>
2021-07-17 18:01:20 +0530

    first commit

---
+++ Drives/about.php
@@ -0,0 +1,180 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<!--favicon-->
+        <link rel="shortcut icon" href="favicon.ico" type="image/icon">
+        <link rel="icon" href="favicon.ico" type="image/icon">
+<title>Drives :: About</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Tillage Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- bootstarp-css -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<!--// bootstarp-css -->
+<!-- css -->
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+<!--// css -->
+<script src="js/jquery-1.11.1.min.js"></script>
+<!--fonts-->
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,800,700,600' rel='stylesheet' type='text/css'>
+<!--/fonts-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/wow.min.js"></script>
+<script>
+	 new WOW().init();
+</script>
+</head>
+<body>
+	<!-- banner -->
+	<div class="banner a-banner">
+		<!-- container -->
+		<div class="container">
+			<div class="header">
+				<div class="head-logo">
+					
+				</div>
+				<div class="top-nav">
+					<span class="menu"><img src="images/menu.png" alt=""></span>
+					<ul class="nav1">
+						<li class="hvr-sweep-to-bottom">
+								<a href="../Homepage/index.php">Jobtracker<i>Goto Placement Homepage</i></a>
+							</li>
+						<li class="hvr-sweep-to-bottom">
+								<a href="index.php">Home<i>Get the Overview of the Site</i></a>
+							</li>
+							<li class="hvr-sweep-to-bottom active">
+								<a href="about.php">About<i>About Us and our Cheif Architects</i></a>
+							</li>
+							<li class="hvr-sweep-to-bottom">
+								<a href="products.php">Our Drives<i>Our Drives</i></a>
+							</li>
+							
+							<li class="hvr-sweep-to-bottom">
+								<a href="mail.php">Mail Us<i>Get in Touch with us Instantly</i></a>
+							</li>
+							<div class="clearfix"></div>
+						<div class="clearfix"> </div>
+					</ul>
+					<!-- script-for-menu -->
+							 <script>
+							   $( "span.menu" ).click(function() {
+								 $( "ul.nav1" ).slideToggle( 300, function() {
+								 // Animation complete.
+								  });
+								 });
+							</script>
+						<!-- /script-for-menu -->
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+		<!-- //container -->
+	</div>
+	<!-- //banner -->
+	<!-- about -->
+	<div class="about-top">
+	
+		</div>
+		<!-- //container -->
+	</div>
+	<!-- //about -->
+	<!-- about-bottom -->
+	<div class="about-bottom">
+		<!-- container -->
+		<div class="container">
+			<h3 class="wow fadeInRight animated" style="visibility: visible;">A New Generation of Jobtracker</h3>
+			<div class="about-bottom-grids">
+	
+				<div class="col-md-6 about-bottom-left about-bottom-right wow fadeInRight animated animated" data-wow-delay="0.5s" style="visibility: visible; -webkit-animation-delay: 0.5s;">
+					<h4>We Promote</h4>
+					<p>We wish to bring to your kind notice that for promoting the campus employment opportunities for students of the Engineering Colleges located in the rural areas, we have formed a joint understanding with many other Institutions on full commitment basis. This association is called “ACCESS” - ASSOCIATION OF ENGINEERING COLLEGES FOR THE JOINT CAMPUS EMPLOYMENT SERVICES AND SELECTIONS, with its headquarters in our Campus. Through this association we can organize pool recruitment drives with large footfall as per the requirement.
+					<span>We strongly believe that the Placement process is not just a yearly affair, but is an on-going relationship between an Institute and the organization. I would like to thank the industry for its kind patronage and successful associations with us year after year and look forward to similar 'tie-ups' in future. </span>
+					</p>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+		<!-- //container -->
+	</div>
+	<!-- //about-bottom -->
+	<!-- team -->
+	<div class="team">
+		<!-- container -->
+		<!--<div class="container">
+			<div class="team-info">
+				<h3 class="wow bounceIn animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">Meet Our Team</h3>
+				<div class="team-grids">
+					<div class="col-md-3 team-grid wow bounceIn animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+						<img src="images/b7.jpg" alt="">
+						<h6>Shantala Suresh</h6>
+						<p> Vice-Principal and HOD CSE Department</p>
+					</div>
+					<div class="col-md-3 team-grid wow bounceIn animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+						<img src="images/b10.jpg" alt="">
+						<h6>Name</h6>
+						<p>Designation</p>
+					</div>
+					<div class="col-md-3 team-grid wow bounceIn animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+						<img src="images/b8.jpg" alt="">
+						<h6>Name</h6>
+						<p> Designation</p>
+					</div>
+					<div class="col-md-3 team-grid wow bounceIn animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+						<img src="images/b9.jpg" alt="">
+						<h6>Rohan Raikwar</h6>
+						<p>PTO Incharge</p>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+		</div> -->
+		<!-- //container -->
+	</div>
+	<!-- //team -->
+	<!-- footer -->
+		<div class="footer">
+			<!-- container -->
+			<div class="container">
+				<div class="col-md-6 footer-left  wow fadeInLeft animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+					<ul>
+						<li>
+							<a href="index.php">Home</a>
+						</li>
+						<li>
+							<a href="about.php">About</a>
+						</li>
+						<li>
+							<a href="products.php">Our Drives</a>
+						</li>
+						
+						<li>
+							<a href="mail.php">Mail Us</a>
+						</li>
+					</ul>
+					<form>
+						<input type="text" placeholder="Email" required="">
+						<input type="submit" value="Subscribe">
+					</form>
+				</div>
+				<div class="col-md-3 footer-middle wow bounceIn animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+					
+				</div>
+				
+					
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<!-- //container -->
+		</div>
+		<!-- //footer -->
+	
+					
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<!-- //container -->
+		</div>
+</body>
+</html>
---
+++ Drives/mail.php
@@ -0,0 +1,143 @@
+﻿
+<!DOCTYPE html>
+<html>
+<head>
+	<!--favicon-->
+		<link rel="shortcut icon" href="favicon.ico" type="image/icon">
+		<link rel="icon" href="favicon.ico" type="image/icon">
+<title>Jobtracker| Contact Us</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Tillage Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- css -->
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+<!--// css -->
+<!-- bootstarp-css -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<!--// bootstarp-css -->
+<script src="js/jquery-1.11.1.min.js"></script>
+<!--fonts-->
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,800,700,600' rel='stylesheet' type='text/css'>
+<!--/fonts-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/wow.min.js"></script>
+<script>
+	 new WOW().init();
+</script>
+</head>
+<body>
+	<!-- banner -->
+	<div class="banner a-banner">
+		<!-- container -->
+		<div class="container">
+			<div class="header">
+				<div class="head-logo">
+					
+				</div>
+				<div class="top-nav">
+					<span class="menu"><img src="images/menu.png" alt=""></span>
+					<ul class="nav1">
+						<li class="hvr-sweep-to-bottom">
+								<a href="../Homepage/index.php">Jobtracker<i>Goto Placement Homepage</i></a>
+							</li>
+						<li class="hvr-sweep-to-bottom">
+								<a href="index.php">Home<i>Get the Overview of the Site</i></a>
+								<li class="hvr-sweep-to-bottom">
+									<a href="about.php">About<i>About Us and our Cheif Architects</i></a>
+								</li>
+								<li class="hvr-sweep-to-bottom">
+									<a href="products.php">Our Drives<i>Our Drives</i></a>
+								</li>
+								
+								<li class="hvr-sweep-to-bottom active">
+									<a href="mail.php">Mail Us<i>Get in Touch with us Instantly</i></a>
+								</li>
+						<div class="clearfix"> </div>
+					</ul>
+					<!-- script-for-menu -->
+							 <script>
+							   $( "span.menu" ).click(function() {
+								 $( "ul.nav1" ).slideToggle( 300, function() {
+								 // Animation complete.
+								  });
+								 });
+							</script>
+						<!-- /script-for-menu -->
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+		<!-- //container -->
+	</div>
+	<!-- //banner -->
+	<!-- mail -->
+	<div class="mail">
+		<!-- container -->
+		<div class="container">
+			<div class="map footer-middle wow bounceIn animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+				<iframe src="https://www.google.com/maps/embed/v1/place?q=Sri%20Channabasaveshwara%20Institute%20of%20Technology%2C%20Harivesandra%2C%20Karnataka%2C%20India&key=AIzaSyATLPQdEcx47VybeMCQzR36pLuNuItNKP8"" frameborder="0" style="border:0"></iframe>
+			</div>
+			<div class="mail-grids">
+				<div class="col-md-6 mail-grid-left wow fadeInLeft animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+				
+					<h4>Get In Touch</h4>
+				
+						E-mail: <a href="mailto:jobtracker148@gmail.com">jobtracker.com</a>
+					</p>
+				</div>
+				<div class="col-md-6 contact-form wow fadeInRight animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+					<form>
+						<input type="text" placeholder="Name" required="">
+						<input type="text" placeholder="Email" required="">
+						<input type="text" placeholder="Subject" required="">
+						<textarea placeholder="Message" required=""></textarea>
+						<input type="submit" value="SEND">
+					</form>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+		<!-- //container -->
+	</div>
+	<!-- //mail -->
+	<!-- footer -->
+		<div class="footer">
+			<!-- container -->
+			<div class="container">
+				<div class="col-md-6 footer-left  wow fadeInLeft animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+					<ul>
+						<li>
+							<a href="index.php">Home</a>
+						</li>
+						<li>
+							<a href="about.php">About</a>
+						</li>
+						<li>
+							<a href="products.php">Our Drives</a>
+						</li>
+						
+						<li>
+							<a href="mail.php">Mail Us</a>
+						</li>
+					</ul>
+					<form>
+						<input type="text" placeholder="Email" required="">
+						<input type="submit" value="Subscribe">
+					</form>
+				</div>
+				<div class="col-md-3 footer-middle wow bounceIn animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+					
+				
+				<div class="copyright-right wow fadeInRight animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;">
+					<ul>
+					
+					</ul>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		<!-- //container -->
+	</div>
+</body>
+</html>
